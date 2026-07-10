59690a2c0e4dc631f46b9a6ad6eead7dda5187fe
JauniKapauni <riches-goods.54@icloud.com>
2026-07-10 14:19:45 +0200

    feat: made config

--- src/main/java/de/jaunikapauni/axflags/AxFlags.java
+++ src/main/java/de/jaunikapauni/axflags/AxFlags.java
@@ -7,10 +7,15 @@ public final class AxFlags extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        saveDefaultConfig();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public boolean getFlag(String world, String flag){
+        return getConfig().getBoolean("worlds." + world + "." + flag, true);
     }
 }
