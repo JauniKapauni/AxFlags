6127179f932b1642db2ee6d2e407a6a0c3ebd710
JauniKapauni <riches-goods.54@icloud.com>
2026-07-10 14:26:43 +0200

    feat: add BlockPlaceListener

--- src/main/java/de/jaunikapauni/axflags/AxFlags.java
+++ src/main/java/de/jaunikapauni/axflags/AxFlags.java
@@ -1,5 +1,6 @@
 package de.jaunikapauni.axflags;
 
+import listener.BlockPlaceListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AxFlags extends JavaPlugin {
@@ -8,6 +9,7 @@ public final class AxFlags extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(new BlockPlaceListener(this), this);
     }
 
     @Override
---
+++ src/main/java/listener/BlockPlaceListener.java
@@ -0,0 +1,22 @@
+package listener;
+
+import de.jaunikapauni.axflags.AxFlags;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+public class BlockPlaceListener implements Listener {
+
+    AxFlags reference;
+    public BlockPlaceListener(AxFlags reference){
+        this.reference = reference;
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent e){
+        String world = e.getBlock().getWorld().getName();
+        if(!reference.getFlag(world, "build")){
+            e.setCancelled(true);
+        }
+    }
+}
